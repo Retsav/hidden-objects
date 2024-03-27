cf19cbce852e839f6ca8598aba418a3ad8903123
Dominik Dadański <ddadanski@edu.cdv.pl>
2024-03-27 22:41:30 +0100

    Diorama change during state change

---
+++ Assets/Core/Code/Game/DioramaLevel.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DioramaLevel : MonoBehaviour
+{
+    public Diorama exploringDiorama;
+    public Diorama searchingDiorama;
+}
